8a6d8990dc666b2f429ba772a3f7fcabbc06feef
Dominika Jelonek <jelonekdominika@gmail.com>
2020-11-25 19:56:34 +0100

    Adding switching between tabs - now only order displays - booking still empty

--- project-pizzeria/src/js/app.js
+++ project-pizzeria/src/js/app.js
@@ -1,6 +1,6 @@
 import { Product } from './components/Product.js';
 import { Cart } from './components/Cart.js';
-import { select, settings } from './settings.js';
+import { select, settings, classNames } from './settings.js';
 
 const app = {
   initMenu: function () {
@@ -49,6 +49,50 @@ const app = {
     });
   },
 
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*TODO: GET PAGE ID FROM HREF*/
+        
+        const href = clickedElement.getAttribute('href');
+
+        /*extract id from href constant*/
+        
+        const id = href.replace("#", "");
+        
+        
+        /*TODO: ACTIVATE PAGE*/
+        thisApp.activatePage(id);
+        
+      });
+    }
+
+
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    };
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.getAttribute('id') == pageId);
+    };
+
+  },
+    
+  
+
   init: function () {
     const thisApp = this;
     ////console.log('*** App starting ***');
@@ -57,8 +101,10 @@ const app = {
     ////console.log('settings:', settings);
     ////console.log('templates:', templates);
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    
   },
 };
 
