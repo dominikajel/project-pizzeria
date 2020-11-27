af0180695f470ad8a7bf9f58302bb00b2b796857
Dominika Jelonek <jelonekdominika@gmail.com>
2020-11-27 07:21:43 +0100

    Change getElements method in AmountWidget

--- project-pizzeria/src/js/components/AmountWidget.js
+++ project-pizzeria/src/js/components/AmountWidget.js
@@ -2,28 +2,28 @@ import { select, settings } from '../settings.js';
 import { BaseWidget } from './BaseWidget.js'; 
 
 export class AmountWidget extends BaseWidget {
-  constructor(element) {
-    super(element, settings.amountWidget.defaultValue);
+  constructor(wrapper) {
+    super(wrapper, settings.amountWidget.defaultValue);
 
     const thisWidget = this;
-    thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
+    thisWidget.getElements();
+    // thisWidget.value = settings.amountWidget.defaultValue;
+    // thisWidget.setValue(thisWidget.input.value);
     thisWidget.initActions();
     ////console.log('AmountWidget:', thisWidget);
     ////console.log('constructor arguments:', element);
   }
 
-  getElements(element) {
+  getElements() {
     const thisWidget = this;
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(
+    
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(
       select.widgets.amount.input
     );
-    thisWidget.linkDecrease = thisWidget.element.querySelector(
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(
       select.widgets.amount.linkDecrease
     );
-    thisWidget.linkIncrease = thisWidget.element.querySelector(
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(
       select.widgets.amount.linkIncrease
     );
   }
